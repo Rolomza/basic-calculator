c438a9fe30c39e7c1801a3b26f257757dac75b73
Augusto <augusto.robles.mza@gmail.com>
2021-03-14 15:55:31 -0300

    operate function

--- scripts.js
+++ scripts.js
@@ -2,3 +2,5 @@ const add = (a,b) => a + b;
 const substract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
+
+const operate = (operator, num1, num2) => operator(num1,num2);
